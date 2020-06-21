3704f1da898610314472da5b52f20bcb68d31002
Brian Chiem <brianfamily5@gmail.com>
2020-06-20 20:27:34 -0600

    Add image carousel for home page

---
+++ src/Components/HomepageCarousel/homecarousel.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const photos = [
+  {
+    name: "photo 1",
+    url: "Photos/building.png",
+  },
+  {
+    name: "photo 2",
+    url: "Photos/building.png",
+  },
+  {
+    name: "photo 3",
+    url: "Photos/building.png",
+  },
+];
+
+export default class HomeCarousel extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      fade: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      arrows: true,
+      slidesToScroll: 1,
+      className: "slides",
+    };
+    return (
+      <div>
+        <Slider {...settings}>
+          {photos.map((photos) => {
+            return (
+              <div>
+                <img width="100%" src={photos.url} />
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
+    );
+  }
+}
--- src/Components/Pages/HomePage/homepage.js
+++ src/Components/Pages/HomePage/homepage.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import "./homepage.css";
 import NavBarExport from "../../Navbar/NavbarExport";
 import Footer from "../../Footer/footer";
-
+import HomeCarousel from "../../HomepageCarousel/homecarousel";
 export default class HomePage extends Component {
   render() {
     return (
       <div>
         <NavBarExport />
         <div className="homepage">
-          <div></div>
+          <div>
+            <HomeCarousel />
+          </div>
           <header>Kobe</header>
           <div style={{ height: "2000px" }}></div>
         </div>
