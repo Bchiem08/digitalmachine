021ca98f7b98e0b620313df4d792f3705741cbdf
Brian Chiem <brianfamily5@gmail.com>
2020-06-26 21:43:50 -0600

    Clean files

--- src/Components/contactusfooter/contactfooter.js
+++ src/Components/contactusfooter/contactfooter.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./contactfooter.css";
+import { Link } from "react-router-dom";
 
 export default class ContactFooter extends Component {
   render() {
@@ -8,7 +9,9 @@ export default class ContactFooter extends Component {
         <div className="contact-box">
           <header className="contactheader">Have any Questions?</header>
           <p className="contact-text">Feel free to contact us here</p>
-          <button className="contact-button">Contact Us</button>
+          <Link to={"/contactus"}>
+            <button className="contact-button">Contact Us</button>
+          </Link>
         </div>
       </div>
     );
