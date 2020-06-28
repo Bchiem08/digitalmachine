141441360dfff73c9bcd9b16151e020a0b554635
Brian Chiem <brianfamily5@gmail.com>
2020-06-27 19:44:46 -0600

    Add Service page

--- src/Components/Pages/ServicesPage/services.js
+++ src/Components/Pages/ServicesPage/services.js
@@ -8,6 +8,17 @@ export default class ServicesPage extends Component {
     return (
       <div>
         <NavbarExport />
+        <div className="service-headerimage" />
+        <div className="header-textbox">
+          <header className="service-header">Services</header>
+          <p className="service-text">
+            We offer a range of services ar Digital Machine. Including milling
+            and turning. Our group of expert machinist ensure the highest
+            quality for all our clients.
+          </p>
+        </div>
+        <div className="service-image" />
+
         <Footer />
       </div>
     );
