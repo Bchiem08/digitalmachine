5c5067639293520f40320de64587ebeebb398b7d
Brian Chiem <brianfamily5@gmail.com>
2020-06-29 15:53:38 -0600

    Add photos page background

--- src/Components/Pages/PhotosPage/photos.js
+++ src/Components/Pages/PhotosPage/photos.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import NavbarExport from "../../Navbar/NavbarExport";
 import Footer from "../../Footer/footer";
+import "./photos.css";
 
 export default class PhotosPage extends Component {
   render() {
     return (
       <div>
         <NavbarExport />
+        <div className="photospage-background">
+          <div className="photos-headerbox">
+            <header className="photos-header">Photos</header>
+          </div>
+        </div>
         <Footer />
       </div>
     );
