94e5be8452924c7fecf7dbb5da8f84384aa9fe15
Brian Chiem <brianfamily5@gmail.com>
2020-06-29 20:22:38 -0600

    Add section 1 of homepage

---
+++ src/Components/Pages/HomePage/Homepagesections/machiningneeds.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./homepagesections.css";
+
+export default class MachiningNeeds extends Component {
+  render() {
+    return (
+      <div className="box">
+        <header className="homepage-header">
+          Contact us for all your general machining needs
+        </header>
+        <div className="homepage-line" />
+        <p className="homepage-text">
+          Since 2000 we have put our clients first. We keep out quality
+          standards high and provide the best consistency. Our quality
+          management system is established to ensure our clients only get the
+          best quality.
+        </p>
+        <div style={{ height: "40px" }} />
+        <button className="homepage-button">Contact Us</button>
+        <div style={{ height: "50px" }} />
+      </div>
+    );
+  }
+}
--- src/Components/Pages/HomePage/homepage.js
+++ src/Components/Pages/HomePage/homepage.js
@@ -3,17 +3,18 @@ import "./homepage.css";
 import NavBarExport from "../../Navbar/NavbarExport";
 import Footer from "../../Footer/footer";
 import HomeCarousel from "../../HomepageCarousel/homecarousel";
+import MachiningNeeds from "./Homepagesections/machiningneeds";
 export default class HomePage extends Component {
   render() {
     return (
       <div>
         <NavBarExport />
         <div className="homepage">
-          <div>
+          <div style={{ marginBottom: "100px" }}>
             <HomeCarousel />
           </div>
-          <header>Kobe</header>
-          <div style={{ height: "2000px" }}></div>
+          <MachiningNeeds />
+          <div></div>
         </div>
         <Footer />
       </div>
