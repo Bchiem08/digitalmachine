6009c690fb8054d72c65b0f08248b22abfefb646
Brian Chiem <brianfamily5@gmail.com>
2020-07-09 19:38:22 -0600

    Add photos to photo page

--- src/Components/Pages/PhotosPage/Gallery/photosdata.js
+++ src/Components/Pages/PhotosPage/Gallery/photosdata.js
@@ -4,4 +4,25 @@ export const photos = [
   {
     image: imagePath + "/quality.png",
   },
+  {
+    image: imagePath + "/Machine_2.JPG",
+  },
+  {
+    image: imagePath + "/Stair.JPG",
+  },
+  {
+    image: imagePath + "/Exterior_1.JPG",
+  },
+  {
+    image: imagePath + "/Shopimage_1.JPG",
+  },
+  {
+    image: imagePath + "/Shopimage_3.JPG",
+  },
+  {
+    image: imagePath + "/Machine_1.JPG",
+  },
+  {
+    image: imagePath + "/Rob.JPG",
+  },
 ];
